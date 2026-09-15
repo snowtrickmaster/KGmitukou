23b2bfd894d3877ce3108304d86c729f3b67e44f
snowtrickmaster <snow.trickmaster@gmail.com>
2026-09-15 11:57:47 +0900

    Update app.js

--- app.js
+++ app.js
@@ -280,7 +280,7 @@ async function downloadPDF() {
     const el = document.getElementById('quote-paper');
 
     const canvas = await html2canvas(el, {
-      scale: 1.0,
+      scale: 0.9,
       useCORS: true,
       backgroundColor: '#ffffff'
     });
